e00594134dba5cc7806580b0f40dbe4b92cae126
ibbgomes <16123612+ibbgomes@users.noreply.github.com>
2025-07-31 22:55:51 +0100

    Remove application tests dead code

--- src/Application.Tests/Constants/Hashes.cs
+++ src/Application.Tests/Constants/Hashes.cs
@@ -9,24 +9,4 @@ internal static class Hashes
     /// The MD5 hash.
     /// </summary>
     internal const string MD5 = "72513B3437F09B729BBC6011949DEBF9";
-
-    /// <summary>
-    /// The SHA1 hash.
-    /// </summary>
-    internal const string SHA1 = "BAF173AB3FCBDDEE35D97884FEA9AFF37A47D96F";
-
-    /// <summary>
-    /// The SHA256 hash.
-    /// </summary>
-    internal const string SHA256 = "2AD4BD189E207F62C3EF73F253CA72F528F0B37593D1FE7CEA1D69136397C26C";
-
-    /// <summary>
-    /// The SHA384 hash.
-    /// </summary>
-    internal const string SHA384 = "754B2A1B1AC5B9194C3E1A3CBCF9FBE1D8FC41328601D011926EE30CD9164E42EB009642AFEA9A809446025999539B62";
-
-    /// <summary>
-    /// The SHA512 hash.
-    /// </summary>
-    internal const string SHA512 = "EEF9995ABC271935A9FE5BE4C8C758FE03BECB40E77A42D0FEB83F5C191551FF896AE4139F496D40E904F29896EB61CFA7B275775C2B4763CE40FF5D9EE25870";
 }
