d737e5d256275b6c66dbe6c0daf243532bdeffaa
ibbgomes <16123612+ibbgomes@users.noreply.github.com>
2026-04-01 22:11:23 +0100

    Rename application tests constants

---
+++ src/Application.Tests/Constants/Input.cs
@@ -0,0 +1,12 @@
+﻿namespace Hashx.Application.Tests;
+
+/// <summary>
+/// Defines input used in unit tests.
+/// </summary>
+internal static class Input
+{
+    /// <summary>
+    /// The file path.
+    /// </summary>
+    internal const string FilePath = "Data/mock.json";
+}
